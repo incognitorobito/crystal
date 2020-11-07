b1dc13844ff5daebed8af335d0f6e4a500b89e82
Mark Raymond Jr <markrjr@live.com>
2020-11-07 11:43:07 -0800

    Restore evented and add winsock version to constants

--- src/crystal/system/socket.cr
+++ src/crystal/system/socket.cr
@@ -3,16 +3,15 @@ require "c/netdb"
 require "c/netinet/in"
 require "c/netinet/tcp"
 require "c/sys/socket"
+require "io/evented"
 
 module Crystal::System::Socket
 
 
 end
 
-
 {% if flag?(:unix) %}
   require "c/sys/un"
-  require "io/evented"
 {% elsif flag?(:win32) %}
   require "c/sys/winsock2"
 {% else %}
--- src/lib_c/x86_64-windows-msvc/c/sys/winsock2.cr
+++ src/lib_c/x86_64-windows-msvc/c/sys/winsock2.cr
@@ -3,6 +3,7 @@ require "c/winnt"
 @[Link("Ws2_32")]
 lib LibC
 
+  WINSOCK_VERSION = 0x20000002
   WSA_IO_PENDING = 997;
 
   type WINSOCK = UInt64
